37be68829fbdc6338b922e8c2a4ef82a041e2ee6
Gunvir Dhesi <gdhesi2005@gmail.com>
2024-08-31 14:58:05 -0400

    fixed css problem

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 const myEnvVar = process.env.MY_ENV_VARIABLE;
 
 const path = require('path');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 
